f67c17cf31dc292ba747218e4978ffec309e19bd
diksed <diksed00@gmail.com>
2022-07-17 15:19:04 +0300

    List data from database

---
+++ CarDealer_Website/ListCars.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using CarDealer_Website.Classes;
+
+namespace CarDealer_Website
+{
+    public partial class ListCars : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlCommand commandList = new SqlCommand("Select * from TableCar where CarConfirmation=@pcon",SqlConnectionClass.conn);
+            SqlConnectionClass.CheckConn();
+            commandList.Parameters.AddWithValue("@pcon", true);
+            SqlDataReader dr = commandList.ExecuteReader();
+            DataList1.DataSource = dr;
+            DataList1.DataBind();
+        }
+    }
+}

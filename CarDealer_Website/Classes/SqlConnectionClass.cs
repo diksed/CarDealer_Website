395e6314da3602e35d731c70a3083238e1201e51
diksed <diksed00@gmail.com>
2022-07-17 14:46:55 +0300

    SqlConnection

---
+++ CarDealer_Website/Classes/SqlConnectionClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace CarDealer_Website.Classes
+{
+    public class SqlConnectionClass
+    {
+        public static SqlConnection conn = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=CarDealer;Integrated Security=True");
+        public static void CheckConn()
+        {
+            if (conn.State == System.Data.ConnectionState.Closed)
+                conn.Open();
+            else
+            {
+
+            }
+        }
+    }
+}

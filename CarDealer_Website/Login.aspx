f1efe5e88a6838aca9d483dd1fda4e22842c8969
diksed <diksed00@gmail.com>
2022-07-17 17:28:50 +0300

    Login section added

---
+++ CarDealer_Website/Login.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Layout.Master" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="CarDealer_Website.Login" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <form id="form1" runat="server">
+        <asp:TextBox ID="tboxMail" runat="server" Height="30px" TextMode="Email" Width="241px"></asp:TextBox>
+        <br />
+        <br />
+        <asp:TextBox ID="tboxPassword" runat="server" Height="30px" TextMode="Password" Width="241px"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="btnLogin" runat="server" Height="44px" Text="Login" Width="143px" OnClick="btnLogin_Click" />
+    </form>
+</asp:Content>
---
+++ CarDealer_Website/Login.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using CarDealer_Website.Classes;
+using System.Data;
+
+namespace CarDealer_Website
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            SqlCommand commLogin = new SqlCommand("SELECT * FROM TableUser where UserMail=@pmail and UserPassword = @ppassword",SqlConnectionClass.conn);
+            SqlConnectionClass.CheckConn();
+            string shaPassword = Sha256Converter.ComputeSha256Hash(tboxPassword.Text);
+            commLogin.Parameters.AddWithValue("@pmail", tboxMail.Text);
+            commLogin.Parameters.AddWithValue("@ppassword", shaPassword);
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(commLogin);
+            da.Fill(dt);
+            if (dt.Rows.Count > 0)
+                Response.Write("Successfully logged in!");
+            else
+            {
+                Response.Write("Mail address or password is incorrect!");
+            }
+        }
+    }
+}
